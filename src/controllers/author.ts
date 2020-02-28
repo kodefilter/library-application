0d0964e224f7b80f0b7639097def09c210fb5186
prrdee <18732242+prrdee@users.noreply.github.com>
2020-02-28 15:05:36 +0200

    add Author service

---
+++ src/controllers/author.ts
@@ -0,0 +1,91 @@
+import { Request, Response, NextFunction } from 'express'
+
+import Author from '../models/Author'
+import AuthorService from '../services/author'
+
+import {
+  NotFoundError,
+  BadRequestError,
+  InternalServerError,
+} from '../helpers/apiError'
+
+// POST /authors
+export const createAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { firstName, lastName } = req.body
+
+    const author = new Author({
+      firstName,
+      lastName,
+    })
+
+    await AuthorService.create(author)
+    res.json(author)
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      next(new BadRequestError('Invalid Request', error))
+    } else {
+      next(new InternalServerError('Internal Server Error', error))
+    }
+  }
+}
+
+// PUT /authors/:authorId
+export const updateAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const update = req.body
+    const authorId = req.params.authorId
+    const updatedAuthor = await AuthorService.update(authorId, update)
+    res.json(updatedAuthor)
+  } catch (error) {
+    next(new NotFoundError('Author not found', error))
+  }
+}
+
+// DELETE /authors/:authorId
+export const deleteAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await AuthorService.deleteAuthor(req.params.authorId)
+    res.status(204).end()
+  } catch (error) {
+    next(new NotFoundError('Author not found', error))
+  }
+}
+
+// GET /authors/:authorId
+export const findById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res.json(await AuthorService.findById(req.params.authorId))
+  } catch (error) {
+    next(new NotFoundError('Author not found', error))
+  }
+}
+
+// GET /authors
+export const findAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res.json(await AuthorService.findAll())
+  } catch (error) {
+    next(new NotFoundError('Books not found', error))
+  }
+}
---
+++ src/services/author.ts
@@ -0,0 +1,57 @@
+import Author, { AuthorDocument } from '../models/Author'
+
+function create(author: AuthorDocument): Promise<AuthorDocument> {
+  return author.save()
+}
+
+function findById(authorId: string): Promise<AuthorDocument> {
+  return Author.findById(authorId)
+    .exec() // .exec() will return a true Promise
+    .then(author => {
+      if (!author) {
+        throw new Error(`Author ${authorId} not found`)
+      }
+      return author
+    })
+}
+
+function findAll(): Promise<AuthorDocument[]> {
+  return Author.find()
+    .sort({ name: 1 })
+    .exec() // Return a Promise
+}
+
+function update(
+  authorId: string,
+  update: Partial<AuthorDocument>
+): Promise<AuthorDocument | null> {
+  return Author.findById(authorId)
+    .exec()
+    .then(author => {
+      if (!author) {
+        throw new Error(`Author ${authorId} not found`)
+      }
+
+      if (update.firstName) {
+        author.firstName = update.firstName
+      }
+      if (update.lastName) {
+        author.lastName = update.lastName
+      }
+
+      // Add more fields here if needed
+      return author.save()
+    })
+}
+
+function deleteAuthor(authorId: string): Promise<AuthorDocument | null> {
+  return Author.findByIdAndDelete(authorId).exec()
+}
+
+export default {
+  create,
+  findById,
+  findAll,
+  update,
+  deleteAuthor,
+}
