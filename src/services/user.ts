cd2da8d146053b65692fc7fb54826c0f801a20c6
prrdee <18732242+prrdee@users.noreply.github.com>
2020-03-03 10:09:06 +0200

    add service for user

---
+++ src/services/user.ts
@@ -0,0 +1,60 @@
+import User, { UserDocument } from '../models/User'
+
+function create(user: UserDocument): Promise<UserDocument> {
+  return user.save()
+}
+
+function findById(userId: string): Promise<UserDocument> {
+  return User.findById(userId)
+    .exec() // .exec() will return a true Promise
+    .then(user => {
+      if (!user) {
+        throw new Error(`User ${userId} not found`)
+      }
+      return user
+    })
+}
+
+function findAll(): Promise<UserDocument[]> {
+  return User.find()
+    .sort({ name: 1 })
+    .exec() // Return a Promise
+}
+
+function update(
+  userId: string,
+  update: Partial<UserDocument>
+): Promise<UserDocument | null> {
+  return User.findById(userId)
+    .exec()
+    .then(user => {
+      if (!user) {
+        throw new Error(`User ${userId} not found`)
+      }
+
+      if (update.firstName) {
+        user.firstName = update.firstName
+      }
+      if (update.lastName) {
+        user.lastName = update.lastName
+      }
+      if (update.email) {
+        user.email = update.email
+      }
+
+      // Add more fields here if needed
+      return user.save()
+    })
+}
+
+function deleteUser(userId: string): Promise<UserDocument | null> {
+  return User.findByIdAndDelete(userId).exec()
+}
+
+export default {
+  create,
+  findById,
+  findAll,
+  update,
+  deleteUser,
+}
