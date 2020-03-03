09c76cbea504916f9e9d6a2e7fac101a97d87854
prrdee <18732242+prrdee@users.noreply.github.com>
2020-03-03 10:15:29 +0200

    add controller for user

---
+++ src/controllers/user.ts
@@ -0,0 +1,92 @@
+import { Request, Response, NextFunction } from 'express'
+
+import User from '../models/User'
+import UserService from '../services/user'
+
+import {
+  NotFoundError,
+  BadRequestError,
+  InternalServerError,
+} from '../helpers/apiError'
+
+// POST /users
+export const createAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const body = req.body
+
+    const user = new User({
+      firstName: body.firstName,
+      lastName: body.lastName,
+      email: body.email,
+    })
+
+    UserService.create(user)
+    res.json(user)
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      next(new BadRequestError('Invalid Request', error))
+    } else {
+      next(new InternalServerError('Internal Server Error', error))
+    }
+  }
+}
+
+// PUT /users/:userId
+export const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const update = req.body
+    const userId = req.params.userId
+    const updatedUser = await UserService.update(userId, update)
+    res.json(updatedUser)
+  } catch (error) {
+    next(new NotFoundError('User not found', error))
+  }
+}
+
+// DELETE /users/:userId
+export const deleteUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await UserService.deleteUser(req.params.userId)
+    res.status(204).end()
+  } catch (error) {
+    next(new NotFoundError('User not found', error))
+  }
+}
+
+// GET /users/:userId
+export const findById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res.json(await UserService.findById(req.params.userId))
+  } catch (error) {
+    next(new NotFoundError('User not found', error))
+  }
+}
+
+// GET /users
+export const findAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    res.json(await UserService.findAll())
+  } catch (error) {
+    next(new NotFoundError('Books not found', error))
+  }
+}
