b0027faceca179e8f6c0ca1ef67a56a02d232ebc
prrdee <18732242+prrdee@users.noreply.github.com>
2020-03-19 19:44:46 +0200

    add snackbar from material ui for success message

--- client/src/components/NotificationBar/index.tsx
+++ client/src/components/NotificationBar/index.tsx
@@ -1,37 +1,53 @@
-import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { Alert, AlertTitle } from '@material-ui/lab';
+import React, { useState } from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../types';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-      '& > * + *': {
-        marginTop: theme.spacing(2),
-      },
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
     },
-  }),
-);
+  },
+}));
 
 export default function NotificationBar() {
   const classes = useStyles()
 
   const {errorMessage,successMessage} = useSelector((state: AppState) => state.notification.message)
+  const [open, setOpen] = useState(false);
+
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
 
   if (errorMessage === '' && successMessage === '') {
     return null 
   }
 
-  return (
+  if (errorMessage){
+    return (<div className={classes.root}> <Alert severity="error">{errorMessage}</Alert> </div>)
+  } else {
+    return (
     <div className={classes.root}>
-      { errorMessage ? <Alert severity="error"> <AlertTitle>Error</AlertTitle>{errorMessage}</Alert>
-      : <Alert severity="success"> <AlertTitle>Success</AlertTitle>{successMessage}</Alert>
-      }
-    </div>
-  );
+      <Snackbar open={true} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">{successMessage}</Alert>
+      </Snackbar>
+    </div>)
+    }
 }
 
 
