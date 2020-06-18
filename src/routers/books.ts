fb7c66b784f5ba0f7735a7ba987a9d93bc95eb04
prrdee <18732242+prrdee@users.noreply.github.com>
2020-06-18 15:31:55 +0300

    add checkAndVerifyToken

--- src/routers/books.ts
+++ src/routers/books.ts
@@ -10,13 +10,15 @@ import {
   unborrowBook,
 } from '../controllers/book'
 import passport from 'passport'
+// Authentication Service
+import AuthenticationService from '../services/authentication'
 
 const router = express.Router()
 
 // Every path we define here will get /api/v1/books prefix
 router.put('/borrow',borrowBook) //user needs to go to specifc book to borrow and unborrow
 router.put('/unborrow', unborrowBook)
-router.get('/', findAll)
+router.get('/', AuthenticationService.checkAndVerifyToken, findAll)
 router.get('/:bookId', findById)
 router.put('/:bookId', updateBook)
 router.delete('/:bookId', deleteBook)
