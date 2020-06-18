d17b8bdc253cbb2fe849090b2d1cd6909c2b54c5
prrdee <18732242+prrdee@users.noreply.github.com>
2020-06-18 16:17:51 +0300

    add any type

--- src/routers/authRoutes.ts
+++ src/routers/authRoutes.ts
@@ -6,7 +6,7 @@ const router = express.Router()
 
 // Every path we define here will get '/auth' prefix
 
-router.post('/google', passport.authenticate('google-token', {session: false}), function(req,res,next) {
+router.post('/google', passport.authenticate('google-token', {session: false}), function(req: any,res,next) {
     if(!req.user) {
         return res.status(401).send('User Not Authenticated')
     }
