584aee672e4a78871928f7d818b327c1c31d50e6
prrdee <18732242+prrdee@users.noreply.github.com>
2020-06-18 15:32:35 +0300

    add some types

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -1,9 +1,12 @@
 import jwt from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express'
 
-function checkAndVerifyToken (req, res, next){
-    const token = req.headers['x-auth-token']
+function checkAndVerifyToken (req: Request, res: Response, next: NextFunction){
+
+    const token: any  = req.headers['x-auth-token']
+
     if(typeof token !==  'undefined') {
-        jwt.verify(token,process.env['JWT_SECRET'] as string, (err, authData) => {
+        jwt.verify(token,process.env['JWT_SECRET'] as string, (err: Error, authData: any) => {
             if(err) {            
                 res.sendStatus(500)
             } else {
@@ -15,7 +18,7 @@ function checkAndVerifyToken (req, res, next){
     }
 }
 
-function signAndSendToken (req,res){
+function signAndSendToken (req: any, res: Response){
     req.token = jwt.sign({ id: req.auth.id, email: req.auth.email },process.env['JWT_SECRET'] as string,{ expiresIn: 60 * 120})
     res.setHeader('x-auth-token', req.token)
     return res.status(200).send(JSON.stringify(req.user))
