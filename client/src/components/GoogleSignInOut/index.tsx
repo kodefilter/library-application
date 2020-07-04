2e2ea5c928f0acd6195da4a27e1b4727ef8d594c
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-04 19:44:33 +0300

    add logout button

--- client/src/components/GoogleSignInOut/index.tsx
+++ client/src/components/GoogleSignInOut/index.tsx
@@ -1,28 +1,48 @@
 import React from 'react'
-import { GoogleLogin } from 'react-google-login'
-import { useDispatch } from 'react-redux'
+import { GoogleLogin, GoogleLogout } from 'react-google-login'
+import { useDispatch, useSelector } from 'react-redux'
 import { signInUserThunk } from '../../redux/actions/user'
+import { AppState } from '../../types'
 
 export default function GoogleSignInOut() {
   const dispatch = useDispatch()
 
+  const currentUser = useSelector((state: AppState) => state.user.currentUser)
+
   const login = (response: any) => {
     dispatch(signInUserThunk(response))
+  }
+
+  const logout = () => {
+    // dispatch(signOutUserThunk()) need to implement this thunk
   }
 
   const loginFailure = () => {
     alert('Login Failed')
   }
 
+  const logoutFailure = () => {
+    alert('Logout Failed')
+  }
+
   return (
     <>
-      <GoogleLogin
-        clientId="659114991649-egsmdi2p7p7fu360cpq4i7evom0beq6c.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={login}
-        onFailure={loginFailure}
-        cookiePolicy={'single_host_origin'}
-      />
+      {currentUser ? (
+        <GoogleLogout
+          clientId="659114991649-egsmdi2p7p7fu360cpq4i7evom0beq6c.apps.googleusercontent.com"
+          buttonText="Logout"
+          onLogoutSuccess={logout}
+          onFailure={logoutFailure}
+        ></GoogleLogout>
+      ) : (
+        <GoogleLogin
+          clientId="659114991649-egsmdi2p7p7fu360cpq4i7evom0beq6c.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={login}
+          onFailure={loginFailure}
+          cookiePolicy={'single_host_origin'}
+        />
+      )}
     </>
   )
 }
