87feb5e444ee9e7b034be1f13bd843eb5bc6c503
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-06 17:11:45 +0300

    fix typo

--- client/src/redux/reducers/user.ts
+++ client/src/redux/reducers/user.ts
@@ -1,22 +1,19 @@
-import {
-    UserState,
-    UserActions,
-    ADD_CURRENT_USER,
-  } from '../../types'
-  
-  export default function author(
-    state: UserState = { users: [], currentUser: { cart: [], firstName: '', lastName: '' } },
-    action: UserActions
-  ): UserState {
-    switch (action.type) {
+import { UserState, UserActions, ADD_CURRENT_USER } from '../../types'
 
-    case ADD_CURRENT_USER: {
-      const { user } = action.payload
-      return {...state, users: [ ...state.users, user], currentUser: user}
-    }
-    
-    default:
-      return state
-    }
+export default function user(
+  state: UserState = {
+    users: [],
+    currentUser: { cart: [], firstName: '', lastName: '' },
+  },
+  action: UserActions
+): UserState {
+  switch (action.type) {
+  case ADD_CURRENT_USER: {
+    const { user } = action.payload
+    return { ...state, users: [...state.users, user], currentUser: user }
   }
-  
+
+  default:
+    return state
+  }
+}
