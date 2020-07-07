00a17d85cac8f2034fb03f4f385b47e154d7905a
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-07 23:49:36 +0300

    add Landing page component

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,10 +1,33 @@
-import GoogleSignInOut from '../components/GoogleSignInOut'
 import React from 'react'
+import GoogleLogin from 'react-google-login'
+import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { signInUserThunk } from '../redux/actions/user'
 
 export default function Landing() {
+  const history = useHistory()
+
+  const dispatch = useDispatch()
+
+  const login = (response: any) => {
+    dispatch(signInUserThunk(response))
+    history.push('/home')
+  }
+
+  const loginFailure = () => {
+    alert('Login Failed')
+  }
+
   return (
     <>
-      <GoogleSignInOut />
+      <GoogleLogin
+        clientId="659114991649-egsmdi2p7p7fu360cpq4i7evom0beq6c.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={login}
+        onFailure={loginFailure}
+        cookiePolicy={'single_host_origin'}
+      />
     </>
   )
 }
+// need to put login button here and on success history push to home screen
