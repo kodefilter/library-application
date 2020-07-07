31c589140d42e2c761ef290f2748c2826bc0826f
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-07 17:55:24 +0300

    add landing page

---
+++ client/src/pages/Landing.tsx
@@ -0,0 +1,10 @@
+import GoogleSignInOut from '../components/GoogleSignInOut'
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <>
+      <GoogleSignInOut />
+    </>
+  )
+}
