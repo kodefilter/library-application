e9fb4a244b051affd01a3e546584d40404579a8a
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-11 19:25:56 +0300

    make service fatter

--- client/src/services/authors.tsx
+++ client/src/services/authors.tsx
@@ -1,22 +1,38 @@
 import { Author } from '../types'
 import Cookies from 'js-cookie'
 import axios from 'axios'
+import { Dispatch } from 'redux'
+import { getAllAuthors, addNotification } from '../redux/actions'
 const baseUrl = 'http://localhost:3001/api/v1/authors'
 
 let myHeaders = new Headers()
 myHeaders.append('x-auth-token', Cookies.get('access-cookie') as string)
 
-
-const getAll = () => {
-    return axios({ method: 'get', url: baseUrl, headers: myHeaders })
+const getAll = async (dispatch: Dispatch) => {
+  try {
+    const response = await axios.get(baseUrl, {
+      headers: myHeaders,
+    })
+    dispatch(getAllAuthors(response.data))
+  } catch (error) {
+    dispatch(
+      addNotification({
+        errorMessage: `This Error happened ${error}`,
+        successMessage: '',
+      })
+    )
+  }
 }
 
-
 const create = (newAuthor: Author) => {
-    return axios({ method: 'post', url: baseUrl, data: newAuthor, headers: myHeaders })
+  return axios({
+    method: 'post',
+    url: baseUrl,
+    data: newAuthor,
+    headers: myHeaders,
+  })
 }
 
 // delete request is going to delete entry in the database and then returns 204 status
-
 
 export default { getAll, create }
