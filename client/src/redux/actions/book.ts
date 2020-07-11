394b44418fc0cd03a955065edefe309f17a275ef
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-11 23:50:29 +0300

    order actions better

--- client/src/redux/actions/book.ts
+++ client/src/redux/actions/book.ts
@@ -12,21 +12,21 @@ import {
   BookFormValues,
 } from '../../types'
 
-export const getAllBooks = ( books: Book[]): BookActions => {
+export const getAllBooks = (books: Book[]): BookActions => {
   return {
     type: GET_ALL_BOOKS,
     payload: {
-      books
-    }
+      books,
+    },
   }
 }
 
-export const borrowUnborrowBook = ( book: Book): BookActions => {
+export const createBook = (book: Book): BookActions => {
   return {
-    type: BORROW_UNBORROW_BOOK,
+    type: CREATE_BOOK,
     payload: {
-      book
-    }
+      book,
+    },
   }
 }
 
@@ -39,10 +39,9 @@ export const removeBook = (book: Book): BookActions => {
   }
 }
 
-
-export const createBook = (book: Book): BookActions => {
+export const borrowUnborrowBook = (book: Book): BookActions => {
   return {
-    type: CREATE_BOOK,
+    type: BORROW_UNBORROW_BOOK,
     payload: {
       book,
     },
@@ -54,29 +53,29 @@ export const createBook = (book: Book): BookActions => {
  this way these actions will look more clean as I will have more actions
  added here in the future*/
 
-export function unborrowBookThunk(book: Book){
+export function unborrowBookThunk(book: Book) {
   return (dispatch: Dispatch) => {
-    return LendingService.unBorrow(book,dispatch)
+    return LendingService.unBorrow(book, dispatch)
   }
 }
 
-export function borrowBookThunk(book: Book){
+export function borrowBookThunk(book: Book) {
   return async (dispatch: Dispatch) => {
     return LendingService.borrow(book, dispatch)
   }
 }
 
-export function removeBookThunk(book: Book){
+export function removeBookThunk(book: Book) {
   return async (dispatch: Dispatch) => {
-      return BookService.deleteThis(book, dispatch)
+    return BookService.deleteThis(book, dispatch)
   }
 }
 
 /*here BookFromValues is a type of book but it do not require _id
 because that is created by the database which we will use to delete book*/
-export function addBookThunk(book: BookFormValues){
+export function addBookThunk(book: BookFormValues) {
   return async (dispatch: Dispatch) => {
-      return BookService.create(book, dispatch)
+    return BookService.create(book, dispatch)
   }
 }
 
