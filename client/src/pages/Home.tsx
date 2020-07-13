809ce8105db670ec18dd39491312f59204a35329
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-13 17:09:12 +0300

    move dispatch to gridlist

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,23 +1,13 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import React from 'react'
 import ApplicationBar from '../components/ApplicationBar'
 import BookGridList from '../components/BookGridList'
 import NotificationBar from '../components/NotificationBar'
-import { fetchBooksThunk } from '../redux/actions/book'
-import AuthorTableList from '../components/AuthorTableList'
 
 export default function Home() {
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(fetchBooksThunk())
-  }, [dispatch])
-
   return (
     <>
       <ApplicationBar />
       <NotificationBar />
-      <AuthorTableList />
       <BookGridList />
     </>
   )
