ac5edfc873c7f0907813bbed8e25376274dc4ccd
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-13 17:10:01 +0300

    add dispatch fetchBooks

--- client/src/components/BookGridList/index.tsx
+++ client/src/components/BookGridList/index.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import { useSelector } from 'react-redux';
-import { AppState } from '../../types';
-import BookCard from '../BookCard';
-import BookForm from '../BookForm';
-import AuthorForm from '../AuthorForm/intex';
+import React, { useEffect } from 'react'
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import ListSubheader from '@material-ui/core/ListSubheader'
+import { useSelector, useDispatch } from 'react-redux'
+import { AppState } from '../../types'
+import BookCard from '../BookCard'
+import BookForm from '../BookForm'
+import AuthorForm from '../AuthorForm/intex'
+import { fetchBooksThunk } from '../../redux/actions'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,51 +21,40 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     gridList: {
       width: '100%',
-      height: '100%'
+      height: '100%',
     },
     icon: {
       color: 'rgba(255, 255, 255, 0.54)',
     },
-  }),
-);
+  })
+)
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
 export default function BookGridList() {
-  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchBooksThunk())
+  }, [dispatch])
+
   const items = useSelector((state: AppState) => state.book.items)
 
-  console.log('Rached here at gridlist',items)
-  
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <div className={classes.root}>
       <GridList cellHeight={300} className={classes.gridList} cols={4}>
         <GridListTile key="Subheader" cols={8} style={{ height: 'auto' }}>
-          <ListSubheader component="div"><BookForm /><AuthorForm /></ListSubheader>
+          <ListSubheader component="div">
+            <BookForm />
+            <AuthorForm />
+          </ListSubheader>
         </GridListTile>
         {items.map(book => (
           <GridListTile key={book.title}>
-            <BookCard key={book.title} book={book}/>
+            <BookCard key={book.title} book={book} />
           </GridListTile>
         ))}
       </GridList>
     </div>
-  );
+  )
 }
