a99e41108e54eadfa1770058f4042acf91b7255e
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-13 17:06:22 +0300

    add route for AuthorTableList

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Landing from './pages/Landing'
+import AuthorTableList from './components/AuthorTableList'
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Landing} />
     <Route exact path="/home" component={Home} />
+    <Route exact path="/authors" component={AuthorTableList} />
   </Switch>
 )
 
