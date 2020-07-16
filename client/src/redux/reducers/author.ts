9e88484265c3513c353b204b00547eff241e8deb
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-16 22:07:07 +0300

    add remove author reducer case

--- client/src/redux/reducers/author.ts
+++ client/src/redux/reducers/author.ts
@@ -1,28 +1,33 @@
 import {
-    AuthorState,
-    AuthorActions,
-    CREATE_AUTHOR,
-    GET_ALL_AUTHORS,
-  } from '../../types'
-  
-  export default function author(
-    state: AuthorState = { authors: [] },
-    action: AuthorActions
-  ): AuthorState {
-    switch (action.type) {
+  AuthorState,
+  AuthorActions,
+  CREATE_AUTHOR,
+  GET_ALL_AUTHORS,
+  REMOVE_AUTHOR,
+} from '../../types'
 
-    case GET_ALL_AUTHORS: {
-      const { authors } = action.payload
-      return { ...state, authors: authors }
-    }
-  
-    case CREATE_AUTHOR: {
-      const { author } = action.payload
-      return {...state, authors: [ ...state.authors, author]}
-    }
-    
-    default:
-      return state
-    }
+export default function author(
+  state: AuthorState = { authors: [] },
+  action: AuthorActions
+): AuthorState {
+  switch (action.type) {
+  case GET_ALL_AUTHORS: {
+    const { authors } = action.payload
+    return { ...state, authors: authors }
   }
-  
+
+  case CREATE_AUTHOR: {
+    const { author } = action.payload
+    return { ...state, authors: [...state.authors, author] }
+  }
+
+  case REMOVE_AUTHOR: {
+    const { author } = action.payload
+    const newItems = state.authors.filter(item => item._id !== author._id)
+    return { ...state, authors: newItems }
+  }
+
+  default:
+    return state
+  }
+}
