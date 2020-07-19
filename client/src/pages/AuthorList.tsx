3a97452a0ff4cb84d6d57b41b4b2bb927e9e617d
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-19 20:54:41 +0300

    fix import path

--- client/src/pages/AuthorList.tsx
+++ client/src/pages/AuthorList.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ApplicationBar from '../components/ApplicationBar'
 import NotificationBar from '../components/NotificationBar'
-import AuthorTableList from '../components/AuthorTableList'
+import AuthorTableList from '../components/Author/AuthorTableList'
 
 export default function AuthorList() {
   return (
