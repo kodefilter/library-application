fc0e104498fb2f0b550fe1e5335fdc9bed9e633f
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-19 20:57:12 +0300

    add function to append header to all requests

--- client/src/services/books.tsx
+++ client/src/services/books.tsx
@@ -1,60 +1,94 @@
 import { BookFormValues, Book } from '../types'
 import Cookies from 'js-cookie'
 import axios from 'axios'
-import { createBook, addNotification, removeBook, getAllBooks } from '../redux/actions'
+import {
+  createBook,
+  addNotification,
+  removeBook,
+  getAllBooks,
+} from '../redux/actions'
 import { Dispatch } from 'redux'
+
 const baseUrl = 'http://localhost:3001/api/v1/books'
 
-
-const token = Cookies.get('access-cookie') as string
+;(function() {
+  const token = Cookies.get('access-cookie') as string
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token
+  } else {
+    axios.defaults.headers.common['x-auth-token'] = null
+    /*if setting null does not remove `Authorization` header then try     
+        delete axios.defaults.headers.common['Authorization'];
+      */
+  }
+})()
 
 const getAll = async (dispatch: Dispatch) => {
-    try {
-        const response = await axios.get(
-          baseUrl,
-          { 
-            headers: { 'x-auth-token' : token }
-          }
-          )
-        dispatch(getAllBooks(response.data))
-      }
-      catch (error) {
-        dispatch(addNotification({ errorMessage: `This Error happened ${error}`, successMessage: '' }))
-      }
+  try {
+    const response = await axios.get(baseUrl)
+    dispatch(getAllBooks(response.data))
+  } catch (error) {
+    dispatch(
+      addNotification({
+        errorMessage: `This Error happened ${error}`,
+        successMessage: '',
+      })
+    )
+  }
 }
 
 const create = async (book: BookFormValues, dispatch: Dispatch) => {
-    try {
-      const response = await axios({ method: 'POST', url: baseUrl, data: book })
-      dispatch(createBook(response.data))
-    }
-    catch (error) {
-      dispatch(addNotification({ errorMessage: `This Error happened ${error}`, successMessage: '' }))
-    } 
+  try {
+    const response = await axios({ method: 'POST', url: baseUrl, data: book })
+    dispatch(createBook(response.data))
+  } catch (error) {
+    dispatch(
+      addNotification({
+        errorMessage: `This Error happened ${error}`,
+        successMessage: '',
+      })
+    )
+  }
 }
 
 const deleteThis = async (book: Book, dispatch: Dispatch) => {
-    try {
-      const response = await axios({ method: 'DELETE', url: `${baseUrl}/${book._id}`, data: book })
-      dispatch(removeBook(book))
-      dispatch(addNotification({ errorMessage: '', successMessage: `You just deleted ${book.title}`})) 
-    }
-    catch (error) {
-      dispatch(addNotification({ errorMessage: `This Error happened ${error}`, successMessage: '' }))
-    } 
+  try {
+    await axios({ method: 'DELETE', url: `${baseUrl}/${book._id}`, data: book })
+    dispatch(removeBook(book))
+    dispatch(
+      addNotification({
+        errorMessage: '',
+        successMessage: `You just deleted ${book.title}`,
+      })
+    )
+  } catch (error) {
+    dispatch(
+      addNotification({
+        errorMessage: `This Error happened ${error}`,
+        successMessage: '',
+      })
+    )
+  }
 }
 
 const updateThis = async (book: Book, dispatch: Dispatch) => {
   try {
-    const response = await axios({ method: 'PUT', url: `${baseUrl}/${book._id}`, data: book })
+    await axios({ method: 'PUT', url: `${baseUrl}/${book._id}`, data: book })
     //dispatch(updateBook(book)) need to implement this in redux
-    dispatch(addNotification({ errorMessage: '', successMessage: `You just deleted ${book.title}`}))  
+    dispatch(
+      addNotification({
+        errorMessage: '',
+        successMessage: `You just deleted ${book.title}`,
+      })
+    )
+  } catch (error) {
+    dispatch(
+      addNotification({
+        errorMessage: `This Error happened ${error}`,
+        successMessage: '',
+      })
+    )
   }
-  catch (error) {
-    dispatch(addNotification({ errorMessage: `This Error happened ${error}`, successMessage: '' }))
-  }
-
 }
-
 
 export default { getAll, create, deleteThis, updateThis }
