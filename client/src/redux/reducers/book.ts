1c80800757cec083c9b1ddd9c7344549215fad7a
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-23 18:31:07 +0300

    add reducer to filter books

--- client/src/redux/reducers/book.ts
+++ client/src/redux/reducers/book.ts
@@ -6,16 +6,17 @@ import {
   GET_ALL_BOOKS,
   REMOVE_BOOK,
   UPDATE_BOOK,
+  FILTER_ALL_BOOKS,
 } from '../../types'
 
 export default function book(
-  state: BookState = { items: [] },
+  state: BookState = { items: [], filteredItems: [] },
   action: BookActions
 ): BookState {
   switch (action.type) {
   case GET_ALL_BOOKS: {
     const { books } = action.payload
-    return { ...state, items: books }
+    return { ...state, items: books, filteredItems: books }
   }
 
   case CREATE_BOOK: {
@@ -47,6 +48,22 @@ export default function book(
         currentBook.title === book.title
           ? { ...currentBook, isAvailable: book.isAvailable }
           : currentBook
+      ),
+      filteredItems: state.filteredItems.map(currentBook =>
+        currentBook.title === book.title
+          ? { ...currentBook, isAvailable: book.isAvailable }
+          : currentBook
+      ),
+    }
+  }
+
+  case FILTER_ALL_BOOKS: {
+    const { searchText } = action.payload
+
+    return {
+      ...state,
+      filteredItems: state.items.filter(item =>
+        item.title.toLowerCase().includes(searchText.toLowerCase())
       ),
     }
   }
