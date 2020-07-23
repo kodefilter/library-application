4470cd935fa6d0c5d45bc8143d15d56514e48b5a
prrdee <18732242+prrdee@users.noreply.github.com>
2020-07-23 18:32:22 +0300

    add method to search book

--- client/src/components/SearchField/index.tsx
+++ client/src/components/SearchField/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
+import { useDispatch } from 'react-redux'
+import { filterAllBooks } from '../../redux/actions'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,9 +16,19 @@ const useStyles = makeStyles(theme => ({
 export default function SearchField() {
   const classes = useStyles()
 
+  const dispatch = useDispatch()
+
+  const handleSearchTextChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    dispatch(filterAllBooks(event.target.value))
+
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" label="Search" variant="outlined" />
+      <TextField
+        id="outlined-basic"
+        label="Search"
+        variant="outlined"
+        onChange={handleSearchTextChange}
+      />
     </form>
   )
 }
